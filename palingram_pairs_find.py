ccf6455f9ec388146c338080ca5198d95d215a72
gitgud5 <49618496+gitgud5@users.noreply.github.com>
2020-10-22 21:16:35 +0500

    Optimized palingram_pairs_find.py
    Changed the data structure of the file from lists to sets because sets use hashing to locate the item which is very fast whereas lists use indexing which is much slower.

--- palingram_pairs_find.py
+++ palingram_pairs_find.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 """
 Load file by using pre-made module named d_load.py
 The function will return palingram words pair as a list which we will later print.
@@ -11,25 +10,26 @@ from d_load import load
 
 
 def palingrams(file):
-    words = load(file)
-    words_p = []
+    words = set(load(file))
+    words_p = set()
+
+
 
     for word in words:
         r_word = word[::-1]
         length = len(word)
-        if length > 1:
+        if length>1:            
             for j in range(length):
-                if word[j:] == r_word[:length - j] and r_word[length
-                    - j:] in words:
-                    words_p.append((word, r_word[length - j:]))
-                if word[:length - j] == r_word[j:] and r_word[:j] \
-                    in words:
-                    words_p.append((r_word[:j], word))
-
+                if word[j:] == r_word[:length-j] and r_word[length-j:] in words:
+                    words_p.add((word, r_word[length-j:]))
+                if word[:length-j] == r_word[j:] and r_word[:j] in words:
+                    words_p.add((r_word[:j], word))
+                    
+                
     return words_p
 
 
-a = sorted(palingrams('212.txt'))
+a = sorted(palingrams("212.txt"))
 
-for (i, j) in a:
-    print (i, j)
+for i, j in a:
+    print(i,j)
